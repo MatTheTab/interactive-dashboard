678bc996e9422a916b8718dc417aa7101584365a
barte <bartek.pukacki@gmail.com>
2023-05-05 14:42:17 +0200

    Added sidebar section switching

--- ui.R
+++ ui.R
@@ -13,55 +13,66 @@ dashboardPage(
   
   dashboardSidebar(width = 130,
                    sidebarMenu(
-                     menuItem("Dashboard", tabName = "dashboard", icon = icon("gamepad")),
-                     menuItem("Help", tabName = "help", icon = icon("circle-question", class="fa-solid")),
-                     menuItem("About", tabName = "about", icon = icon("circle-info"))
+                     menuItem("Dashboard", tabName="dashboard", icon = icon("gamepad")),
+                     menuItem("Help", tabName="help", icon = icon("circle-question", class="fa-solid")),
+                     menuItem("About", tabName="about", icon = icon("circle-info"))
                    )),
   
   dashboardBody(
-    fluidRow(
-      box(
-        title = "Box title", width = 6, status = "primary",
-        "Box content"
-      ),
-      box(
-        status = "warning", width = 6,
-        "Box content"
-      )
-    ),
-    
-    fluidRow(
-      column(width = 4,
-             box(
-               title = "Title 1", width = NULL, solidHeader = TRUE, status = "primary",
-               "Box content"
+    tabItems(
+      tabItem(tabName="dashboard",
+             fluidRow(
+               box(
+                 title = "Box title", width = 6, status = "primary",
+                 "Box content"
+               ),
+               box(
+                 status = "warning", width = 6,
+                 "Box content"
+               )
              ),
-             box(
-               width = NULL, background = "black",
-               "A box with a solid black background"
+             
+             fluidRow(
+               column(width = 4,
+                      box(
+                        title = "Title 1", width = NULL, solidHeader = TRUE, status = "primary",
+                        "Box content"
+                      ),
+                      box(
+                        width = NULL, background = "black",
+                        "A box with a solid black background"
+                      )
+               ),
+               
+               column(width = 4,
+                      box(
+                        title = "Title 3", width = NULL, solidHeader = TRUE, status = "warning",
+                        "Box content"
+                      ),
+                      box(
+                        title = "Title 5", width = NULL, background = "light-blue",
+                        "A box with a solid light-blue background"
+                      )
+               ),
+               
+               column(width = 4,
+                      box(
+                        title = "Title 2", width = NULL, solidHeader = TRUE,
+                        "Box content"
+                      ),
+                      box(
+                        title = "Title 6", width = NULL, background = "maroon",
+                        "A box with a solid maroon background"
+                      )
+               )
              )
-      ),
       
-      column(width = 4,
-             box(
-               title = "Title 3", width = NULL, solidHeader = TRUE, status = "warning",
-               "Box content"
-             ),
-             box(
-               title = "Title 5", width = NULL, background = "light-blue",
-               "A box with a solid light-blue background"
-             )
       ),
-      
-      column(width = 4,
-             box(
-               title = "Title 2", width = NULL, solidHeader = TRUE,
-               "Box content"
-             ),
-             box(
-               title = "Title 6", width = NULL, background = "maroon",
-               "A box with a solid maroon background"
-             )
+      tabItem(tabName="help",
+             h2("Help section")
+      ),
+      tabItem(tabName="about",
+             h2("About section")
       )
     )
   )
