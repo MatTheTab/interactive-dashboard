315af1244e83a2c7e40544b557530e89b3dadd43
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-05-06 18:17:03 +0200

    Add Safeguard and genre-only similarity plot

--- Visualizations.R
+++ Visualizations.R
@@ -85,12 +85,57 @@ similar_games$similarity <- sapply(1:nrow(norm_similar_games),
                                                           chosen_game_row, i))
 similar_games <- similar_games %>% filter(QueryName != chosen_game)
 similar_games <- similar_games %>% top_n(number_of_bars, similarity)
+second_largest <- sort(similar_games$similarity[similar_games$similarity != Inf],
+                       decreasing = TRUE)[2]
+
+# Replace Inf values with the second largest non-Inf value times 2
+similar_games$similarity[similar_games$similarity == Inf] <- second_largest * 2
+
+
 x_breaks <- seq(floor(min(similar_games$similarity)),
                 ceiling(max(similar_games$similarity)), length.out = 5)
 
 # Create a horizontal bar plot
-p <- ggplot(similar_games, aes(x = similarity, y = reorder(QueryName, similarity))) +
+p <- ggplot(head(similar_games,number_of_bars), aes(x = similarity, y = reorder(QueryName, similarity))) +
   geom_bar(stat = "identity") +
   scale_x_continuous(breaks = x_breaks) +
-  labs(x = "Similarity", y = "Game Name")
+  labs(x = "Similarity", y = "Game Name") +
+  ggtitle("Most Similar Games")
 p
+
+###############################Bar plot based only on genres####################
+
+number_of_bars=15
+
+cluster_value <- games %>% filter(QueryName == chosen_game) %>% pull(cluster)
+similar_games <- games %>% filter(cluster == cluster_value)
+
+chosen_game_row <- which(similar_games$QueryName == chosen_game)
+norm_similar_games<-normalize(similar_games)
+norm_similar_games$QueryID=NULL
+norm_similar_games$ResponseID=NULL
+norm_similar_games[is.na(norm_similar_games)] <- 0
+
+similar_games$similarity <- sapply(1:nrow(norm_similar_games),
+                                   function(i) similarity_only_genres(norm_similar_games, 
+                                                          chosen_game_row, i))
+similar_games <- similar_games %>% filter(QueryName != chosen_game)
+similar_games <- similar_games %>% top_n(number_of_bars, similarity)
+second_largest <- sort(similar_games$similarity[similar_games$similarity != Inf],
+                       decreasing = TRUE)[2]
+
+# Replace Inf values with the second largest non-Inf value times 2
+similar_games$similarity[similar_games$similarity == Inf] <- second_largest * 2
+
+
+x_breaks <- seq(floor(min(similar_games$similarity)),
+                ceiling(max(similar_games$similarity)), length.out = 5)
+
+# Create a horizontal bar plot
+g <- ggplot(head(similar_games,number_of_bars),
+            aes(x = similarity, y = reorder(QueryName, similarity))) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(breaks = x_breaks) +
+  labs(x = "Similarity", y = "Game Name") +
+  ggtitle("Most Similar Games")
+g
